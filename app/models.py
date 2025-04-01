6877e23af1268be394d7bd3c1f96423f6fe8d4a2
Rmrti <mutuma34@gmail.com>
2025-04-01 22:06:52 +0300

    CREATE: power and Heropower Model

--- app/models.py
+++ app/models.py
@@ -13,4 +13,15 @@ class Hero(db.Model):
 
     hero_powers = db.relationship('HeroPower', backref='hero')
 
+class Power(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    hero_powers = db.relationship('HeroPower', backref='power')
+
+class HeroPower(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    strength = db.Column(db.String, nullable=False)
+    hero_id = db.Column(db.Integer, db.ForeignKey('hero.id'), nullable=False)
+    power_id = db.Column(db.Integer, db.ForeignKey('power.id'), nullable=False)
 
