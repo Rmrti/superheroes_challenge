2d65d79d4ae9ec842e8276dfbd075cf1ac95b3c7
Rmrti <mutuma34@gmail.com>
2025-04-01 23:11:53 +0300

    CREATE: route to create hero

--- app/routes.py
+++ app/routes.py
@@ -10,6 +10,17 @@ def get_heroes():
     heroes = Hero.query.all()
     return jsonify([{"id": hero.id, "name": hero.name, "super_name": hero.super_name} for hero in heroes])
 
+#CREATE HEROES
+@app.route('/heroes', methods=['POST'])
+def create_hero():
+    data = request.get_json()
+    if 'name' not in data or 'super_name' not in data:
+        return jsonify({"error": "Missing required fields"}), 400
+    new_hero = Hero(name=data['name'], super_name=data['super_name'])
+    db.session.add(new_hero)
+    db.session.commit()
+    return jsonify({"id": new_hero.id, "name": new_hero.name, "super_name": new_hero.super_name}), 201
+
 # GET /heroes/:id
 
 @app.route('/heroes/<int:hero_id>', methods=['GET'])
