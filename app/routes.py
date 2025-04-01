3a195355378a7633c275036db35d02c2ad15b528
Rmrti <mutuma34@gmail.com>
2025-04-01 22:50:09 +0300

    WRITE: Method to get Hero

--- app/routes.py
+++ app/routes.py
@@ -9,3 +9,29 @@ app = Flask(__name__)
 def get_heroes():
     heroes = Hero.query.all()
     return jsonify([{"id": hero.id, "name": hero.name, "super_name": hero.super_name} for hero in heroes])
+
+# GET /heroes/:id
+
+@app.route('/heroes/<int:hero_id>', methods=['GET'])
+def get_hero(hero_id):
+    hero = Hero.query.get(hero_id)
+    if not hero:
+        return jsonify({'error': 'Hero not found'}), 404
+    return jsonify({
+        'id': hero.id,
+        'name': hero.name,
+        'super_name': hero.super_name,
+        'hero_powers': [
+            {
+                'id': hp.id,
+                'strength': hp.strength,
+                'hero_id': hp.hero_id,
+                'power_id': hp.power_id,
+                'power': {
+                    'id': hp.power.id,
+                    'name': hp.power.name,
+                    'description': hp.power.description
+                }
+            } for hp in hero.hero_powers
+        ]
+    })
