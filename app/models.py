54530ccf34bb88d2b209c63559f87f0054b8ad23
Rmrti <mutuma34@gmail.com>
2025-04-01 22:02:06 +0300

    CREATE: Model class

--- app/models.py
+++ app/models.py
@@ -1,2 +1,16 @@
 
 
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Hero(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    super_name = db.Column(db.String, nullable=False)
+    name = db.Column(db.String, nullable=False)
+    # relationship with super powers
+
+    hero_powers = db.relationship('HeroPower', backref='hero')
+
+
