5b1213ef5a5215a666cb76a4351991e44bf0185a
Rmrti <mutuma34@gmail.com>
2025-04-04 11:21:26 +0300

    REGISTER ROUTES BLUEPRINTS

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,26 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from app.config import Config
 
 # Initialize db and migrate
 db = SQLAlchemy()
 migrate = Migrate()
 
+
+
 def create_app():
     app = Flask(__name__)
-    app.config.from_object("app.config.Config")
+    app.config.from_object(Config)
     db.init_app(app)
     migrate.init_app(app, db)
+    
+    from app.blueprints.heroes import heroes_bp
+    from app.blueprints.powers import powers_bp
+    from app.blueprints.hero_powers import hero_powers_bp
+    
+    app.register_blueprint(heroes_bp, url_prefix='/heroes')
+    app.register_blueprint(powers_bp, url_prefix='/powers')
+    app.register_blueprint(hero_powers_bp, url_prefix='/hero_powers')
     
     return app
